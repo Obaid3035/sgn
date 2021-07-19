6dfa39a9a989376bba4de0dae2526beab4076b68
OBAID  AQEEL <obaidaqeel@OBAIDs-MacBook-Pro.local>
2021-07-19 16:32:09 +0500

    Commit

--- src/index.js
+++ src/index.js
@@ -14,9 +14,9 @@ import './index.css';
 import App from './App';
 
 import { AuthProvider } from './context/AuthContext'
-// axios.defaults.baseURL = 'https://sleepy-savannah-00668.herokuapp.com/';
+axios.defaults.baseURL = 'https://sleepy-savannah-00668.herokuapp.com/';
 // axios.defaults.baseURL = ' https://murmuring-refuge-28649.herokuapp.com/ ';
-axios.defaults.baseURL = 'http://localhost:4000/';
+// axios.defaults.baseURL = 'http://localhost:4000/';
 
 axios.defaults.headers['Content-Type'] = 'application/json';
 
