314cce5d553d47ad742ca270948cd56df1b1390a
OBAID  AQEEL <obaidaqeel@OBAIDs-MacBook-Pro.local>
2021-09-02 16:48:25 +0500

    FIXED

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -3,7 +3,7 @@ import {NavLink} from "react-router-dom";
 
 const NavigationItem = ( props ) => (
         <li className={'nav-item mx-2'}>
-            <NavLink to={props.link} exact={props.exact} className={'nav-link ' + props.class}>{props.children}</NavLink>
+            <NavLink to={props.link} exact={props.exact} className={'nav-link ' + props.class} style={{fontSize: '1.1rem'}}>{props.children}</NavLink>
         </li>
 );
 
